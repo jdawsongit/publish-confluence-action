ec630c1e897a2e0544317ff2f76b8ba61e6ee617
John Dawson <jdawson@eventussystems.com>
2022-10-27 23:46:22 -0500

    Add support for a publish map file
    FIle takes a list of FROM/TO pairs. Useful for repos that have many
    markdown files that need publishing.

--- src/main.py
+++ src/main.py
@@ -4,26 +4,51 @@ import os
 import requests
 import pycmarkgfm
 
-def get_inputs():
-    try:
-        workspace = os.environ['GITHUB_WORKSPACE']
-        envs = {
-            key: os.environ[f'INPUT_{key.upper()}']
-            for key in ['from', 'to', 'cloud', 'user', 'token']
-        }
-    except KeyError as e:
-        raise Exception(f'Missing value for {e}')
-    return (workspace, envs)
+class Config:
+    def __init__(self):
+        self.publish_map = {}
+        try:
+            self.workspace = os.environ['GITHUB_WORKSPACE']
+            self.cloud = os.environ['INPUT_CLOUD']
+            self.user = os.environ['INPUT_USER']
+            self.token = os.environ['INPUT_TOKEN']
+            if 'INPUT_FROM' in os.environ:
+                from_path = os.environ['INPUT_FROM']
+                to_page_id = os.environ['INPUT_TO']
+                self.publish_map[from_path] = to_page_id
+            if 'INPUT_PUBLISH_MAP' in os.environ:
+                publish_map_path = os.path.join(self.workspace, os.environ['INPUT_PUBLISH_MAP'])
+                self.publish_map.update(self.read_publish_map(publish_map_path))
+        except KeyError as e:
+            raise Exception(f'Missing value for {e}')
+    
+    @staticmethod
+    def read_publish_map(publish_map_path: str) -> dict:
+        publish_map = {}
+        print(f"Reading publish map file {publish_map_path}")
+        with open(publish_map_path, "r") as publish_map_file:
+            for line in publish_map_file:
+                line = line.strip()
+                if line.startswith('#') or not line:
+                    continue
+                try:
+                    (from_path, to_page_id) = line.split()
+                except Exception as e:
+                    raise Exception(f"Invalid publish map line: {line}") from e
+                publish_map[from_path] = to_page_id
+        return publish_map
+                
 
-def main():
-    (workspace, envs) = get_inputs()
-    input_path = os.path.join(workspace, envs['from'])
+def publish(config: Config, from_path: str, to_page_id: str):
+    input_path = os.path.join(config.workspace, from_path)
+    print(f"Reading file {from_path}")
     with open(input_path) as f:
         markdown_text = f.read()
 
-    content_url = f"https://{envs['cloud']}.atlassian.net/wiki/rest/api/content/{envs['to']}"
-    print('Fetching old page')
-    response = requests.get(content_url, auth=(envs['user'], envs['token']))
+    content_url = f"https://{config.cloud}.atlassian.net/wiki/rest/api/content/{to_page_id}"
+    print(f'Fetching old page: {to_page_id}')
+    auth = (config.user, config.token)
+    response = requests.get(content_url, auth=auth)
     response.raise_for_status()
     current = response.json()
     print('Rendering')
@@ -41,9 +66,15 @@ def main():
         }
     }
 
-    updated = requests.put(content_url, json=content, auth=(
-        envs['user'], envs['token'])).json()
+    print("Uploading to Confluence")
+    updated = requests.put(content_url, json=content, auth=auth).json()
     link = updated['_links']['base'] + updated['_links']['webui']
     print(f'Uploaded content successfully to page {link}')
+
+def main():
+    config = Config()
+    for (from_path, to_page_id) in config.publish_map.items():
+        publish(config, from_path, to_page_id)
+
 
 main()
